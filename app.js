7dfc3d219fad457e943d2b2502f36b9746d7d821
sai-sourav <saisourav123@gmail.com>
2022-11-30 14:51:46 +0530

    basic html,css and images

---
+++ app.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(4000);
